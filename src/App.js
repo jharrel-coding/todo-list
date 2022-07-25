15d786f14e51540bf45a1b9a755390ca45fb9008
Jason <jason@DESKTOP-TNBN23I.localdomain>
2022-07-24 20:58:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import AddToDoList from './components/AddToDoList';
+import Task from './components/Task';
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([
+  ])
+
+  const addNewTask = (newTask) => {
+    setToDoList( (prevToDoList) => [...prevToDoList,newTask]);
+  }
+
+  const handleOnChange = (e,id) =>{
+    const newToDoList = [...toDoList];
+    newToDoList[id].complete = !newToDoList[id].complete;
+    setToDoList(newToDoList);
+
+  }
+
+  const handleOnClick = (e,id) => {
+    let filteredToDoList = toDoList.filter((task,index) => index !== id );
+    setToDoList(filteredToDoList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>ToDo List</h1>
+        <AddToDoList addNewTask={addNewTask}/>
+        <h2>List of task</h2>
+        {
+          toDoList.map((item,index) =>
+              <Task task = { item } handleOnChange={handleOnChange} key = { index } id={index} handleOnClick={handleOnClick} />
+          )
+        }
+      </div>
   );
 }
 
